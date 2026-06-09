235fb325ebbaca10f0fa1dc44749f50b91a78fb5
Rodrigo Muinhos <rodrigo@example.com>
2026-06-09 10:06:57 -0300

    allow production delivery domain cors

--- backend/src/main/java/com/menfis/delivery/config/WebConfig.java
+++ backend/src/main/java/com/menfis/delivery/config/WebConfig.java
@@ -15,6 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addMapping("/**")
       .allowedOriginPatterns(
         frontendUrl,
+        "https://menfisburguer.com.br",
+        "https://www.menfisburguer.com.br",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://192.168.*.*:3000",
